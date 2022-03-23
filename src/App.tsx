654dc28205cec00b8af7e599460dd27f0247125a
Rafael <faelcabral@hotmail.com>
2022-03-23 11:34:17 -0300

    Questão 22

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import './App.css';
 import Q10BiggerText from './components/Q10BiggerText';
 import Q1String from './components/Q1String';
+import Q22BiggerNumber from './components/Q22BiggerNumber';
 import Q2Uppercase from './components/Q2Uppercase';
 import Q3Lowercase from './components/Q3Lowercase';
 import Q4StringLength from './components/Q4StringLength';
@@ -209,7 +210,10 @@ const App: React.FC = () => {
                 22. Crie um componente que recebe dois números e retorna o maior
                 entre eles
             </h2>
-            <div className="card p-3 mb-4">{/* Insira a resposta aqui */}</div>
+            <div className="card p-3 mb-4">
+                <Q22BiggerNumber value={10} value1={8} />
+                <Q22BiggerNumber value={10} value1={18} />
+            </div>
 
             <h2 className="fs-6">
                 23. Crie um componente que retorna um número inteiro aleatório
