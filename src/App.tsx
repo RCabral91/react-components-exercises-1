29b081a874702bd846543e6ca674566eb3dc2735
Rafael <faelcabral@hotmail.com>
2022-03-23 16:57:11 -0300

    Questão 14

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import './App.css';
 import Q10BiggerText from './components/Q10BiggerText';
+import Q14OnlyFirstUpper from './components/Q14OnlyFirstUpper';
 import Q1String from './components/Q1String';
 import Q2Uppercase from './components/Q2Uppercase';
 import Q3Lowercase from './components/Q3Lowercase';
@@ -155,7 +156,9 @@ const App: React.FC = () => {
                 <br /> Prop 1: MaS QuE LoUcUrA.
                 <br /> Retorno: Mas que loucura.
             </h2>
-            <div className="card p-3 mb-4">{/* Insira a resposta aqui */}</div>
+            <div className="card p-3 mb-4">
+                <Q14OnlyFirstUpper value="MaS QuE LoUcUrA." />
+            </div>
 
             <h2 className="fs-6">
                 15. Crie um componente que recebe uma string e retorna a
