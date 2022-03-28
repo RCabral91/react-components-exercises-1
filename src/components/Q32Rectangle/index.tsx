1917ac893ce0dc270c25150c0cb2c9578f11cc44
Rafael <faelcabral@hotmail.com>
2022-03-28 11:20:43 -0300

    Questão 32

--- src/components/Q32Rectangle/index.tsx
+++ src/components/Q32Rectangle/index.tsx
@@ -1,27 +1,39 @@
-interface IQ32RectangleProps {
+import { useCallback } from 'react';
+
+interface IQ32RectanglesProps {
     x: number;
     width: number;
     height: number;
     color: string;
 }
 
-const Q32Rectangle: React.FC<IQ32RectangleProps> = ({
+const Q32Rectangles: React.FC<IQ32RectanglesProps> = ({
     x,
     width,
     height,
     color,
 }) => {
-    const rectangs = [];
+    const rectangleArray = [] as number[];
 
-    for (let i = 1; i <= x; i += 1) {
-        rectangs.push(
-            <div
-                className="d-flex m-3"
-                style={{ background: color, width, height }}
-            />
-        );
-    }
-    return <p>{rectangs.join('')}</p>;
+    const buildRectanglesArray = useCallback(() => {
+        for (let i = 1; i <= x; i += 1) {
+            rectangleArray.push(i);
+        }
+    }, [x, width, height, color]);
+
+    buildRectanglesArray();
+
+    return (
+        <div className="d-flex">
+            {rectangleArray.map(index => (
+                <div
+                    key={index}
+                    className="m-2"
+                    style={{ height, width, backgroundColor: color }}
+                />
+            ))}
+        </div>
+    );
 };
 
-export default Q32Rectangle;
+export default Q32Rectangles;
