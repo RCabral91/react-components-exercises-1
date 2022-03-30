c437184fa297b722cfdb5912522f76978216ea59
Rafael <faelcabral@hotmail.com>
2022-03-30 10:07:21 -0300

    Questão 43

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,7 @@ import Q22BiggerNumber from './components/Q22BiggerNumber';
 import Q2Uppercase from './components/Q2Uppercase';
 import Q31StarPoint from './components/Q31StarPoint';
 import Q3Lowercase from './components/Q3Lowercase';
+import Q43PlusOne from './components/Q43PlusOne';
 import Q4StringLength from './components/Q4StringLength';
 import Q5FirstChar from './components/Q5FirstChar';
 import Q6LastChar from './components/Q6LastChar';
@@ -403,7 +404,9 @@ const App: React.FC = () => {
                 e um botão. A cada vez que o botão for apertado, o número
                 contido no <b>span</b> deve receber +1
             </h2>
-            <div className="card p-3 mb-4">{/* Insira a resposta aqui */}</div>
+            <div className="card p-3 mb-4">
+                <Q43PlusOne />
+            </div>
 
             <h2 className="fs-6">
                 44. Crie um componente que tem um <b>span</b> vazio, um input de
---
+++ src/components/Q43PlusOne/index.tsx
@@ -0,0 +1,21 @@
+import { useState } from 'react';
+
+const Q43PlusOne: React.FC = () => {
+    const [plusOne, setPlusOne] = useState(0);
+
+    return (
+        <div>
+            <span id="count">{plusOne}</span>
+
+            <button
+                type="submit"
+                id="incrementor"
+                onClick={() => setPlusOne(plusOne + 1)}
+            >
+                Add
+            </button>
+        </div>
+    );
+};
+
+export default Q43PlusOne;
