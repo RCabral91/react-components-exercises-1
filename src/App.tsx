e8c9f34765e6f6ed78345997b09365951dc4b36b
Rafael <faelcabral@hotmail.com>
2022-04-01 15:52:52 -0300

    Questão 47

--- src/App.tsx
+++ src/App.tsx
@@ -20,6 +20,7 @@ import Q36Button from './components/Q36Button';
 import Q38SumInput from './components/Q38SumInput';
 import Q3Lowercase from './components/Q3Lowercase';
 import Q42OneMoreLine from './components/Q42OneLineMore';
+import Q47DolToReal from './components/Q47DolToReal';
 import Q4StringLength from './components/Q4StringLength';
 import Q5FirstChar from './components/Q5FirstChar';
 import Q6LastChar from './components/Q6LastChar';
@@ -477,7 +478,9 @@ const App: React.FC = () => {
                 input convertido para reais. Deve exibir um loading enquanto
                 busca
             </h2>
-            <div className="card p-3 mb-4">{/* Insira a resposta aqui */}</div>
+            <div className="card p-3 mb-4">
+                <Q47DolToReal />
+            </div>
 
             <h2 className="fs-6">
                 48. Crie um componente que tem um span vazio e um botão, ao
---
+++ src/components/Q47DolToReal/index.tsx
@@ -0,0 +1,45 @@
+import axios from 'axios';
+import { useCallback, useState } from 'react';
+
+const api = axios.create();
+
+const Q47DolToReal: React.FC = () => {
+    const [dollar, setDollar] = useState(0);
+    const [result, setResult] = useState(0);
+    const [isLoading, setIsLoading] = useState(false);
+
+    const convertDol = useCallback(async () => {
+        try {
+            setIsLoading(true);
+            const response = await api.get(
+                'https://economia.awesomeapi.com.br/json/last/USD-BRL'
+            );
+            const { bid } = response.data.USDBRL;
+            setResult(dollar * parseFloat(bid));
+        } catch {
+            setResult(0);
+        } finally {
+            setIsLoading(false);
+        }
+    }, [dollar]);
+
+    return (
+        <div>
+            <input
+                className="m-3"
+                type="number"
+                onChange={e => setDollar(parseInt(e.target.value, 10))}
+            />
+            <span>{isLoading ? 'Loading' : result}</span>
+            <button
+                className="btn btn-primary"
+                type="button"
+                onClick={convertDol}
+            >
+                Converter
+            </button>
+        </div>
+    );
+};
+
+export default Q47DolToReal;
