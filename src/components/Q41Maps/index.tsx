8179c9782a577fb0207e3bca4d4885f39b41f070
Rafael <faelcabral@hotmail.com>
2022-04-04 14:13:49 -0300

    Questão 49

---
+++ src/components/Q41Maps/index.tsx
@@ -0,0 +1,13 @@
+import { useCallback, useState } from 'react';
+
+const Q42OneMoreLine: React.FC = () => {
+    return (
+        <div>
+            <input type="text" />
+            <input type="text" />
+            <button type="button">Procurar</button>
+        </div>
+    );
+};
+
+export default Q42OneMoreLine;
